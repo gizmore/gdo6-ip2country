8641f96c249476fa816dcb111b74c7d8880884ac
gizmore <gizmore@wechall.net>
2019-08-07 16:02:51 +0200

    6.10-r10_tooltips

--- lang/ip2country_en.php
+++ lang/ip2country_en.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-'ft_ip2country_installip2c' => '[%s] Install IP to Country',
+'ft_ip2country_installip2c' => 'Install IP to Country',
 'msg_ip2country_installed' => 'IP to country lookup tables have been installed. %s entries created.',
 );
